d3721d0c60a445774a1218ef5b92ad698dd59b39
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-10-29 19:48:14 -0200

    Testando input e print

---
+++ script-python/test1.py
@@ -0,0 +1,12 @@
+from calendar import month
+
+
+name = input('Qual o seu nome')
+print("bem vindo",name)
+
+day = input('Em qual dia você nasceu?')
+month = input('Em qual mês você nasceu?')
+year = input('Em qual ano você nasceu?')
+
+print("Então você nascem em:",day,'/',month,'/',year)
+
