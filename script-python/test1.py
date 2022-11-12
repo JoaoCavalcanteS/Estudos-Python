f3629b65853463e173939c4aa4017848c668ad5e
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-11-12 19:29:45 -0200

    Update test1.py

--- script-python/test1.py
+++ script-python/test1.py
@@ -1,12 +1,18 @@
 from calendar import month
 
 
-name = input('Qual o seu nome')
-print("bem vindo",name)
+# name = input('Qual o seu nome')
+# print("bem vindo",name)
 
-day = input('Em qual dia você nasceu?')
-month = input('Em qual mês você nasceu?')
-year = input('Em qual ano você nasceu?')
+# day = input('Em qual dia você nasceu?')
+# month = input('Em qual mês você nasceu?')
+# year = input('Em qual ano você nasceu?')
 
-print("Então você nascem em:",day,'/',month,'/',year)
+# print("Então você nascem em:",day,'/',month,'/',year)
+
+n1 = input("Diga um numero para eu somar")
+n2 = input('Diga o segundo numero para eu somar')
+soma = n1+n2
+print('A soma dos dois são:',n1+n2)
+input("fim")
 
